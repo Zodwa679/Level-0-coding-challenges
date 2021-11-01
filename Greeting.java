cd9c9ee89d2f3e6c6aca22f415646c3eac83729c
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-01 19:32:01 +0200

    Completed Task0.3

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+public class Greeting {
+
+    static void Hello(String name) {
+
+        System.out.println("Hello " + name + "!");
+
+    }
+
+    public static void main(String args[]) {
+        Hello("Tshepo");
+
+    }
+
+}
