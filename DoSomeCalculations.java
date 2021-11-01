78368a5ef9b6bc1afbe62288458a3b7ca2ffe540
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-01 19:30:35 +0200

    Completed Task0.2

---
+++ DoSomeCalculations.java
@@ -0,0 +1,15 @@
+public class DoSomeCalculations {
+    public static void main(String args[]) {
+        int x = 1 + 1 * 2;
+        int y = (1 + 1) * 2;
+        int z = 1 + (1 * 2);
+        int a = 1 + 1 * 2 / 2;
+        double b = (1 + 1 * 2) / 2;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
