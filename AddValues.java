b3bba42e0111d02e840710b6939e93615c9a6c7d
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-01 19:27:05 +0200

    Completed Task0.1

---
+++ AddValues.java
@@ -0,0 +1,16 @@
+public class AddValues {
+    public static void main(String args[]) {
+        int x = 0;
+        int y = 1;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        x = x + 3;
+        y = y + x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+}
