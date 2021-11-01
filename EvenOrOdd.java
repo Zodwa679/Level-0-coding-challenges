17d1cee9035b2e50dbe5fd449ac0e1c14bedd367
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-01 19:33:28 +0200

    Completed Task0.4

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+public class EvenOrOdd {
+
+    static void isEvenOrOdd(int num) {
+        if (num % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+
+    public static void main(String args[]) {
+        isEvenOrOdd(4);
+
+    }
+
+}
