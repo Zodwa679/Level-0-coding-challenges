66984691e7987304d4fdb152afd88d9ad01b2e6c
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-02 21:59:15 +0200

    used StringBuilder to do the correct concatenation

--- ConvertNumberToTime.java
+++ ConvertNumberToTime.java
@@ -1,22 +1,25 @@
 public class ConvertNumberToTime {
     public static void main(String args[]) {
-        System.out.println(convertNumberTime(65));
+        System.out.println(convertNumberTime(71));
 }
-public static String convertNumberTime(int number){
+public static StringBuilder convertNumberTime(int number){
     int minutes = number % 60;
     int hours = (number - minutes )/60 ;
 
     String minutepluralCheck = ((minutes == 1 )? "minute" : "minutes"); 
     String hourPluralCheck = ((hours == 1) ? "hour " : "hours ");
 
-   String minuteText = minutes + ' ' + minutepluralCheck;
-    String hourText = hours + ' ' + hourPluralCheck;
+   StringBuilder minuteText = new StringBuilder().append(minutes).append(' ').append(minutepluralCheck);
+   StringBuilder hourText = new StringBuilder().append(hours).append(' ').append(hourPluralCheck);
 
    if(hours == 0) {
-         return  hourText + minuteText; 
+         return   minuteText; 
+    }
+    else if(minutes == 0){
+        return hourText;
     }
    else {
-        return (minutes >= 0) ? hourText + minuteText : hourText; 
+        return (minutes >= 1) ? hourText.append(minuteText) : hourText; 
     }
 }
     
