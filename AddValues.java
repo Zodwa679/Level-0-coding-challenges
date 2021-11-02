d3827d9d9f71828a21696c67966f43c198b05acd
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-02 11:25:07 +0200

    Changed the code to a function

--- AddValues.java
+++ AddValues.java
@@ -1,16 +1,14 @@
 public class AddValues {
     public static void main(String args[]) {
-        int x = 0;
-        int y = 1;
+        addvaluesTogether(0, 1);
+    }
 
+    public static void addvaluesTogether(int x, int y){
         System.out.println(x);
         System.out.println(y);
-
         x = x + 3;
         y = y + x;
-
         System.out.println(x);
         System.out.println(y);
     }
-
 }
