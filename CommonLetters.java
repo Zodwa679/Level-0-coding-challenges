0cfb25d66a9cfdef3ff5a08c9554448a9a907670
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-02 21:06:54 +0200

    used arguments instead of the scanner method

--- CommonLetters.java
+++ CommonLetters.java
@@ -1,27 +1,29 @@
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 
 public class CommonLetters {
-        public static void main(String args[])
-        {
-           System.out.println("Enter Your String 1: ");
-           Scanner sc = new Scanner(System.in);
-           String str1 = sc.nextLine();
-           System.out.println("Enter Your String 2: ");
-           String str2 = sc.nextLine();
-    
-           Set<String> str = new HashSet<String>();
-           for(int i=0;i<str1.length();i++){
-               for (int j = 0; j < str2.length(); j++) {
-                  if(str1.charAt(i) == str2.charAt(j)){
-                      str.add(str1.charAt(i)+"");
-                  }
+        public static void main(String[] args) {
+           findCommonChars("HoUse", "computers");    
+        }
+public static void findCommonChars(String string1, String string2){
+    String stringOne = string1.toLowerCase();
+    String stringTwo = string2.toLowerCase();
+    StringBuilder commonLetter = new StringBuilder();
+
+    for(int i = 0; i < stringTwo.length(); i++){
+        String charString1 = Character.toString(stringTwo.charAt(i));
+        for(int j = 0; j < stringOne.length(); j++){
+            String charString2 = Character.toString(stringOne.charAt(j));
+            if(charString1.equals(charString2)){
+                 commonLetter.append(charString1).append(',');
             }
-           }
-    
-            System.out.println(str);
         }
     }
+    System.out.print("common characters:" + " ");
+    System.out.println(commonLetter);
+}
+}
 
+    
+    
     
