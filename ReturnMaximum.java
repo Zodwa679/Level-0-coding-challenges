b50843e5ea8fa2b5f8391e577e45238ac8631390
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-02 11:10:40 +0200

    Removed commented out code

--- ReturnMaximum.java
+++ ReturnMaximum.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 public class ReturnMaximum
 {
    public static void main(String[] args){
@@ -19,22 +18,4 @@ public class ReturnMaximum
  
 
 
-  /*public static int  findmaximum(){
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Please enter the total of your numbers: ");
-      int numbers = sc.nextInt();
-      int maximum = Integer.MIN_VALUE;
-      System.out.println("Please enter " + numbers + " numbers.");
-      for(int a = 0; a < numbers; a++)
-      {
-         int current = sc.nextInt();
-         if(current > maximum)
-         {
-            maximum = current;
-         }
-      }
-      sc.close();
-     return  maximum;
-   }
-}
-*/
+  
