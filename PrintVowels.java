598e344c6fcbb1ef4f66de1836dd46095f57cb30
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-02 22:12:48 +0200

    Removed the scanner method and created a separate function

--- PrintVowels.java
+++ PrintVowels.java
@@ -1,22 +1,17 @@
-import java.util.Scanner;
 
 public class PrintVowels {
     public static void main(String[] args) {
-        String str = null;
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter any String: ");
-		str = sc.nextLine();
-		str = str.toLowerCase();
-
-		System.out.print("Vowels in the given String are:");
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
-					|| str.charAt(i) == 'u') {
-				System.out.print(" " + str.charAt(i));
-			}
-		}
-
+		findVowels("umuzi");
 	}
 
+public static void findVowels(String str){
+	String input = str.toLowerCase();
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o'
+					|| input.charAt(i) == 'u') {
+				System.out.print(" " + input.charAt(i));
+			}
+	}
+}
 }
 
