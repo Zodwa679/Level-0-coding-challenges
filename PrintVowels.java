edbad460627fa0f8805fb896a43e03709a51e64a
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-08 18:31:43 +0200

    inserted the comas in the out put

--- PrintVowels.java
+++ PrintVowels.java
@@ -6,12 +6,22 @@ public class PrintVowels {
 
 public static void findVowels(String str){
 	String input = str.toLowerCase();
-		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o'
-					|| input.charAt(i) == 'u') {
-				System.out.print(" " + input.charAt(i));
-			}
+
+	String prefix = "";
+	String current = "";
+	//String previous = "";
+	//List<Character> chars = new ArrayList<Character>();	
+		
+	for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o'|| input.charAt(i) == 'u') {
+				current = prefix + input.charAt(i);
+				prefix = ",";
+				System.out.print(current);
+				
 	}
+	}
+	//System.out.println(previous);
 }
 }
+
 
