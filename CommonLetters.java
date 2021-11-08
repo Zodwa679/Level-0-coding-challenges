58ea4a5f20049818e31c5e95ba05cd449baf6c1b
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-08 13:06:21 +0200

    inserted spaces between the characters and removed the last comma

--- CommonLetters.java
+++ CommonLetters.java
@@ -1,23 +1,27 @@
 public class CommonLetters {
         public static void main(String[] args) {
-           findCommonChars("HoUse", "computers");    
+           findCommonChars("House", "computers");    
         }
 public static void findCommonChars(String string1, String string2){
     String stringOne = string1.toLowerCase();
     String stringTwo = string2.toLowerCase();
     StringBuilder commonLetter = new StringBuilder();
+    String prefix = "";
+
 
     for(int i = 0; i < stringTwo.length(); i++){
         String charString1 = Character.toString(stringTwo.charAt(i));
         for(int j = 0; j < stringOne.length(); j++){
             String charString2 = Character.toString(stringOne.charAt(j));
             if(charString1.equals(charString2)){
-                 commonLetter.append(charString1).append(',');
-            }
+                 commonLetter.append(prefix).append(" ").append(charString1);
+                 prefix = ",";
+            };
         }
     }
     System.out.print("common characters:" + " ");
-    System.out.println(commonLetter);
+    
+    System.out.print(commonLetter);
 }
 }
 
