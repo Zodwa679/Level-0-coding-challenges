9a955946dfd3bf1c21a49ff29aa7efe265d08d4e
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-11 14:20:43 +0200

    removed the repeated vowels from in the output

--- PrintVowels.java
+++ PrintVowels.java
@@ -1,27 +1,29 @@
-
 public class PrintVowels {
     public static void main(String[] args) {
 		findVowels("umuzi");
 	}
+	
+    public static String uniqueCharacters(String test){
+		String word = test.toLowerCase();
+		String currenString = "";
+         for (int i = 0; i < word.length(); i++){
+            if (currenString.indexOf(word.charAt(i)) == - 1){
+               currenString += word.charAt(i);
+         }
+      }
+    return currenString;
+   }
 
-public static void findVowels(String str){
-	String input = str.toLowerCase();
-
+public static void findVowels(String string){
+	String input = uniqueCharacters(string);
 	String prefix = "";
-	String current = "";
-	//String previous = "";
-	//List<Character> chars = new ArrayList<Character>();	
-		
+	String outPut = "";		
 	for (int i = 0; i < input.length(); i++) {
 			if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o'|| input.charAt(i) == 'u') {
-				current = prefix + input.charAt(i);
+				outPut = prefix + input.charAt(i);
 				prefix = ",";
-				System.out.print(current);
-				
-	}
-	}
-	//System.out.println(previous);
+				System.out.print(outPut);
+			}	
+		}
 }
-}
-
-
+}			
