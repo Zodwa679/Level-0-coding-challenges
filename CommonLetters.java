66300773d1690cb52a1c1456ec22d465e47f1e9b
Lulutho Mgwali <71149511+Zodwa679@users.noreply.github.com>
2021-11-12 15:15:39 +0200

    Removed duplicates from the output

--- CommonLetters.java
+++ CommonLetters.java
@@ -1,27 +1,31 @@
 public class CommonLetters {
         public static void main(String[] args) {
-           findCommonChars("House", "computers");    
+           findCommonChars("House", "Computers");    
         }
 public static void findCommonChars(String string1, String string2){
     String stringOne = string1.toLowerCase();
     String stringTwo = string2.toLowerCase();
-    StringBuilder commonLetter = new StringBuilder();
-    String prefix = "";
-
+    String commonLetter = "";
 
     for(int i = 0; i < stringTwo.length(); i++){
         String charString1 = Character.toString(stringTwo.charAt(i));
         for(int j = 0; j < stringOne.length(); j++){
             String charString2 = Character.toString(stringOne.charAt(j));
             if(charString1.equals(charString2)){
-                 commonLetter.append(prefix).append(" ").append(charString1);
-                 prefix = ",";
+                 commonLetter += charString1;
             };
         }
     }
     System.out.print("Common letters:");
-    
-    System.out.print(commonLetter);
+    String currenString = "";
+    String prefix = "";
+     for (int i = 0; i < commonLetter.length(); i++){
+        if (currenString.indexOf(commonLetter.charAt(i)) == - 1){
+           currenString +=  prefix + " " + commonLetter.charAt(i);
+           prefix = ",";
+     }
+  }
+ System.out.print(currenString);
 }
 }
 
